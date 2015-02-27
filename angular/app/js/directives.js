91183a590dcd4ee008613d56da296ba8708e8586
Binu Raman <binuraman@gmail.com>
2015-02-27 07:21:29 +0530

    Created directives and services that act as a base.

--- angular/app/js/directives.js
+++ angular/app/js/directives.js
@@ -4,8 +4,108 @@
 
 
 angular.module('myApp.directives', [])
-    .directive('appVersion', ['version', function(version) {
-        return function(scope, elm, attrs) {
-            elm.text(version);
+    .directive('bodyWrap', function(Templates) {
+        return {
+            restrict: 'E,A',
+            replace: true,
+            transclude: true,
+            controller: function($scope, $attrs, Core, Templates, LogRegService, $location, $route, $window) {
+                Core.processOutput = function(data, onSuccessCallback, onFailureCallback, onRedirectCallback, onCompleteCallback) {
+                    if((data instanceof Object) && (typeof data.status == 'string')){
+                        if((data.data instanceof Object) && (data.data.user instanceof Object) && typeof data.data.user.id != 'undefined' && parseInt(data.data.user.id) > 0) {
+                            LogRegService.curruser = data.data.user;
+                            if(data.status != 'redirect' && ($location.path() == '/login' || $location.path() == '/register')) {
+                                $location.path('/');
+                            }
+                        } else {
+                            LogRegService.curruser = {};
+                            if((data.status != 'redirect') && ($location.path() != '/login') && ($location.path() != '/register')) {
+                                $location.path('/login');
+                            }
+                        }
+
+                        switch(data.status) {
+                            case 'success':
+                                if(data.data instanceof Object){
+                                    if(typeof onSuccessCallback == 'function') onSuccessCallback.call(this, data.data);
+                                } else {
+                                    if(typeof onSuccessCallback == 'function') onSuccessCallback.call(this, null);
+                                }
+                                break;
+                            case 'redirect':
+                                if(data.redirect instanceof Object){
+                                    if(typeof data.redirect.redirectURL == 'string' && String(data.redirect.redirectURL).trim() != '') {
+                                        if (typeof data.redirect.alertMessage == 'string') {
+                                            alert(data.redirect.alertMessage);
+                                        }
+                                        var targetURL = String(data.redirect.redirectURL).trim();
+                                        if (targetURL.substring(0, 1) == '#') {
+                                            $location.url(targetURL.substring(1));
+                                            $location.replace();
+                                        } else {
+                                            $window.location = targetURL;
+                                        }
+                                    }
+                                    if(typeof onRedirectCallback == 'function') onRedirectCallback.call(this, data.redirect);
+                                } else {
+                                    if(typeof onRedirectCallback == 'function') onRedirectCallback.call(this, null);
+                                }
+                                break;
+                            default:
+                                if(data.error instanceof Object){
+                                    if(typeof onFailureCallback == 'function') onFailureCallback.call(this, data.error);
+                                } else {
+                                    if(typeof onFailureCallback == 'function') onFailureCallback.call(this, null);
+                                }
+                                break;
+                        }
+                        if(typeof onCompleteCallback == 'function') onCompleteCallback.call(this, data);
+                    } else {
+                        LogRegService.curruser = {};
+                        if(data instanceof Object) {
+                            if(data.error instanceof Object){
+                                if(typeof onFailureCallback == 'function') onFailureCallback.call(this, data.error);
+                            } else {
+                                if(typeof onFailureCallback == 'function') onFailureCallback.call(this, null);
+                            }
+                            if(typeof onCompleteCallback == 'function') onCompleteCallback.call(this, data);
+                        } else {
+                            if(typeof onFailureCallback == 'function') onFailureCallback.call(this, null);
+                            if(typeof onCompleteCallback == 'function') onCompleteCallback.call(this, null);
+                        }
+                    }
+                };
+
+
+                $scope.tpl = Templates;
+                $scope.user = function(){
+                    return LogRegService.curruser;
+                };
+
+                //var user = LogRegService.getData('user/profile', {}, function(){
+                //    Core.processOutput(user);
+                //}, function(){
+                //    Core.processOutput(user);
+                //});
+
+                $scope.userLogout = function() {
+                    var user = LogRegService.userLogout(true, function() {
+                        Core.processOutput(user);
+                    }, function(){
+                        Core.processOutput(user);
+                    });
+                }
+            },
+            templateUrl: Templates.pageBody
+        }
+    })
+    .directive('year', [function() {
+        return {
+            replace: true,
+            link: function(scope, elm, attrs){
+                var dt = new Date();
+                scope.yr = dt.getFullYear();
+            },
+            template: "<span>{{yr}}</span>"
         };
     }]);
--- angular/app/js/services.js
+++ angular/app/js/services.js
@@ -6,4 +6,114 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-    .value('version', '0.1');
+    .factory('Core', function() {
+        return {processOutput: function(data, onSuccessCallback, onFailureCallback, onRedirectCallback, onCompleteCallback){}};
+    })
+    .factory('Templates', function(){
+        return {
+            pageLogin: frontEndAppPath + 'partials/login.html',
+            pageRegister: frontEndAppPath + 'partials/register.html',
+            pageDashboard: frontEndAppPath + 'partials/dashboard.html',
+
+            pageBody: frontEndAppPath + 'partials/body.html',
+            pageHeader: frontEndAppPath + 'partials/header.html',
+            pageFooter: frontEndAppPath + 'partials/footer.html',
+
+            frontEndAppLocation: frontEndAppPath,
+            backEndCakeLocation: backEndCakePath
+        };
+    })
+    .factory('LogRegService', function($http, $q){
+        var httpRequestCanceler = $q.defer();
+        var user = {
+            curruser: {},
+            cancelAndRenewRequest: function(){
+                httpRequestCanceler.resolve();
+                httpRequestCanceler = $q.defer()
+//                console.log(httpRequestCanceler.notify());
+            },
+            userLogin: function(data, onSuccessCallback, onFailureCallback){
+                var params = addRandomSeed({});
+                var result = {}, targetURL = backEndCakePath + 'user/login';
+                this.cancelAndRenewRequest();
+                $http.post(targetURL, data, {timeout: httpRequestCanceler.promise, params: params})
+                    .success(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onSuccessCallback == 'function') onSuccessCallback.apply(this, arguments);
+                    })
+                    .error(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onFailureCallback == 'function') onFailureCallback.apply(this, arguments);
+                    });
+                return result;
+            },
+            userLogout: function(redirectToLogin, onSuccessCallback, onFailureCallback){
+                var params = addRandomSeed({});
+                var result = {}, targetURL = backEndCakePath + 'user/logout';
+                if(redirectToLogin) targetURL += '/1';
+                this.cancelAndRenewRequest();
+                $http.get(targetURL, {timeout: httpRequestCanceler.promise, params: params})
+                    .success(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onSuccessCallback == 'function') onSuccessCallback.apply(this, arguments);
+                    })
+                    .error(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onFailureCallback == 'function') onFailureCallback.apply(this, arguments);
+                    });
+                return result;
+            },
+            userRegister: function(data, onSuccessCallback, onFailureCallback){
+                var params = addRandomSeed({});
+                var result = {}, targetURL = backEndCakePath + 'user/register';
+                this.cancelAndRenewRequest();
+                $http.post(targetURL, data, {timeout: httpRequestCanceler.promise, params: params})
+                    .success(function(data, status){
+                        //if(data instanceof Object && typeof data.status == 'string' && data.status == 'success' && data.data instanceof Object) {
+                        //    data.data = reverseSanitize(data.data);
+                        //}
+                        angular.copy(data, result);
+                        if(typeof onSuccessCallback == 'function') onSuccessCallback.apply(this, arguments);
+                    })
+                    .error(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onFailureCallback == 'function') onFailureCallback.apply(this, arguments);
+                    });
+                return result;
+            },
+            getData: function(APIURL, getvars, onSuccessCallback, onFailureCallback){
+                if(!(getvars instanceof Object)) getvars = {};
+                getvars = addRandomSeed(getvars);
+                var result = {}, targetURL = backEndCakePath + APIURL;
+                this.cancelAndRenewRequest();
+                $http.get(targetURL, {timeout: httpRequestCanceler.promise, params: getvars})
+                    .success(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onSuccessCallback == 'function') onSuccessCallback.apply(this, arguments);
+                    })
+                    .error(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onFailureCallback == 'function') onFailureCallback.apply(this, arguments);
+                    });
+                return result;
+            },
+            postData: function(APIURL, postvars, getvars, onSuccessCallback, onFailureCallback){
+                if(!(getvars instanceof Object)) getvars = {};
+                getvars = addRandomSeed(getvars);
+                var result = {}, targetURL = backEndCakePath + APIURL;
+                this.cancelAndRenewRequest();
+                $http.post(targetURL, postvars, {timeout: httpRequestCanceler.promise, params: params})
+                    .success(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onSuccessCallback == 'function') onSuccessCallback.apply(this, arguments);
+                    })
+                    .error(function(data, status){
+                        angular.copy(data, result);
+                        if(typeof onFailureCallback == 'function') onFailureCallback.apply(this, arguments);
+                    });
+                return result;
+            }
+        };
+        return user;
+    })
+;
