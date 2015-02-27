e8c652afb38e7af84e88c8480311be770f39207c
Binu Raman <binuraman@gmail.com>
2015-02-27 07:24:08 +0530

    Created proper routes, and added controllers and view files. Made the login part completely functional.

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -1,5 +1,14 @@
 'use strict';
+if(typeof frontEndAppPath == 'undefined') var frontEndAppPath = '';
+if(typeof backEndAppPath == 'undefined') var backEndAppPath = '../../cakephp/app/';
+if(typeof backEndAPIPath == 'undefined') var backEndAPIPath = '../../cakephp/api/';
+if(typeof backEndCakePath == 'undefined') var backEndCakePath = '../../cakephp/';
 
+function addRandomSeed(data) {
+    if(typeof data != "object" || data == null) data = {};
+    data.rand = Math.random()*10000;
+    return data;
+}
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
@@ -10,7 +19,8 @@ angular.module('myApp', [
         'myApp.controllers'
     ])
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.when('/login', {templateUrl: frontEndAppPath+'partials/login.html', controller: 'LogRegController'});
+        $routeProvider.when('/register', {templateUrl: frontEndAppPath+'partials/register.html', controller: 'LogRegController'});
+        $routeProvider.when('/', {templateUrl: frontEndAppPath+'partials/dashboard.html', controller: 'Dashboard'});
+        $routeProvider.otherwise({redirectTo: '/'});
     }]);
--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -3,9 +3,77 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MyCtrl1', [function() {
+    .controller('LogRegController', ['$scope', 'Core', 'LogRegService', function($scope, Core, LogRegService) {
+        var user = LogRegService.getData('user/profile', {}, function(){
+            Core.processOutput(user);
+        }, function(){
+            Core.processOutput(user);
+        });
 
+        $scope.logreguser = {};
+        $scope.master = {
+            username: '',
+            password: ''
+        };
+
+        $scope.resetFormSubmittedAndErrors = function(fieldname){
+            $scope.formSubmitted = false;
+            if($scope.formResponseFailure instanceof Object) {
+                if($scope.formResponseFailure.mainform instanceof Object) delete $scope.formResponseFailure.mainform;
+                if($scope.formResponseFailure[fieldname] instanceof Object) delete $scope.formResponseFailure[fieldname];
+            }
+        };
+
+        $scope.update = function(logreguser) {
+            $scope.errorDisplayAllowed = true;
+            $scope.master = angular.copy(logreguser);
+            if($scope.logregform.$invalid) {
+                $scope.formSubmitted = false;
+                return false;
+            } else {
+                $scope.formSubmitted = true;
+                return true;
+            }
+        };
+
+        $scope.userRegister = function() {
+            var user = LogRegService.userRegister($scope.master, function(){
+                Core.processOutput(user, null, function (errors){
+                    $scope.formResponseFailure = errors;
+                }, null, function(){
+                    $scope.formSubmitted = false;
+                });
+            }, function () {
+                Core.processOutput(user, null, function (errors){
+                    $scope.formResponseFailure = errors;
+                }, null, function(){
+                    $scope.formSubmitted = false;
+                });
+            });
+        };
+
+        $scope.userLogin = function() {
+            var user = LogRegService.userLogin($scope.master, function(){
+                Core.processOutput(user, null, function (errors){
+                    $scope.formResponseFailure = errors;
+                }, null, function(){
+                    $scope.formSubmitted = false;
+                });
+            }, function () {
+                Core.processOutput(user, null, function (errors){
+                    $scope.formResponseFailure = errors;
+                }, null, function(){
+                    $scope.formSubmitted = false;
+                });
+            });
+        };
     }])
-    .controller('MyCtrl2', [function() {
-
-    }]);
+    
+    .controller('Dashboard', ['$scope', 'Core', 'LogRegService', function($scope, Core, LogRegService) {
+        var user = LogRegService.getData('user/profile', {}, function(){
+            Core.processOutput(user);
+        }, function(){
+            Core.processOutput(user);
+        });
+    }])
+;
